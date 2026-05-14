b9d4724150411d96d37626f3b1fc562bc8116124
dAeb099 <deabdullichismailich099@gmail.com>
2026-05-14 08:16:20 +0500

    added deleting todos

--- app.js
+++ app.js
@@ -4,30 +4,40 @@ const todoList = document.getElementById("todo-list");
 
 let todos = [];
 
-function renderTodo(text) {
+function renderTodo(todo) {
   todoList.innerHTML += `
-    <li class="todo-item">
-      <span class="todo-text">${text}</span>
-      <button class="delete-btn">×</button>
+    <li class="todo-item" id="todo-${todo.id}">
+      <span class="todo-text ${todo.isCompleted ? "completed" : ""}">${todo.text}</span>
+      <button class="delete-btn" onclick="{
+        deleteTodo(${todo.id})
+      }">×</button>
     </li>
   `;
+}
+
+function deleteTodo(id) {
+  let pressedTodo = document.getElementById(`todo-${id}`);
+  pressedTodo.remove();
+  todos.splice(id, id);
 }
 
 function addTodo() {
   todoList.innerHTML = "";
   todos.forEach((todo) => {
-    renderTodo(todo.text);
+    renderTodo(todo);
   });
 }
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   let inputValue = input.value.trim();
-  todos.push({
-    id: todos.length,
-    text: inputValue,
-    isCompleted: false,
-  });
-  addTodo(inputValue);
-  input.value = "";
+  if (inputValue.trim() !== "") {
+    todos.push({
+      id: todos.length,
+      text: inputValue,
+      isCompleted: false,
+    });
+    addTodo(inputValue);
+    input.value = "";
+  }
 });
